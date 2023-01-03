89bf21bcede8c513d65459514898644453266901
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2023-01-03 18:29:09 +0530

    filter items favonly & all

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -5,27 +5,65 @@ import '../providers/products.dart';
 
 import '../widgets/product_item.dart';
 
-class ProductOverviewPage extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewPage extends StatefulWidget {
   const ProductOverviewPage({super.key});
+
+  @override
+  State<ProductOverviewPage> createState() => _ProductOverviewPageState();
+}
+
+class _ProductOverviewPageState extends State<ProductOverviewPage> {
+  bool _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.more_vert),
+            onSelected: (selectedVal) {
+              setState(() {
+                if (selectedVal == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorites,
+                child: Text('Only Favorites'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Show All'),
+              ),
+            ],
+          )
+        ],
       ),
-      body: const ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
 
 class ProductsGrid extends StatelessWidget {
-  const ProductsGrid({super.key});
+  const ProductsGrid(this.showFavs, {super.key});
+
+  final bool showFavs;
 
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
