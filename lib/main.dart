dd6d57ef588c05d6180f6fd2292f3d2830345e9c
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2023-01-03 11:27:28 +0530

    product overview page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './pages/products_overview_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -14,23 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyShop'),
-      ),
-      body: const Center(
-        child: Text('Let\'s build a shop!'),
-      ),
+      home: ProductOverviewPage(),
     );
   }
 }
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    super.key,
+  });
+
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      footer: GridTileBar(
+        backgroundColor: Colors.black45,
+        leading: IconButton(
+          icon: const Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: const Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
