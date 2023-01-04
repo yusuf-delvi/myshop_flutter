e293edd09c29a7ee1fc858e31435821a3f90d694
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2023-01-04 20:06:36 +0530

    orders page

---
+++ lib/pages/orders_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/orders.dart' show Orders;
+
+import '../widgets/order_item.dart';
+
+class OrdersPage extends StatelessWidget {
+  const OrdersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Orders'),
+      ),
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+      ),
+    );
+  }
+}
