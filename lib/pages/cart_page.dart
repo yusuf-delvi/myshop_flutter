68c258a67da997cd292bf0a28e8bdeb0df6be22d
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2023-01-04 19:55:38 +0530

    order now btn

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 
 import '../widgets/cart_item.dart';
 
@@ -37,7 +38,13 @@ class CartPage extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Provider.of<Orders>(context, listen: false).addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount,
+                      );
+                      cart.clear();
+                    },
                     child: const Text('ORDER NOW'),
                   ),
                 ],
