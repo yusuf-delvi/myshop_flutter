186515cfd59df9cee1bce67caf4ae3970287df59
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2023-01-04 20:32:44 +0530

    app drawer widget

---
+++ lib/widgets/app_drawer.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../pages/orders_page.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(children: [
+        AppBar(
+          title: const Text('Hello Friends'),
+          automaticallyImplyLeading: false,
+        ),
+        const Divider(),
+        ListTile(
+          leading: const Icon(Icons.shop),
+          title: const Text('Shop'),
+          onTap: () {
+            Navigator.of(context).pushReplacementNamed('/');
+          },
+        ),
+        const Divider(),
+        ListTile(
+          leading: const Icon(Icons.payment),
+          title: const Text('Orders'),
+          onTap: () {
+            Navigator.of(context).pushReplacementNamed(OrdersPage.routeName);
+          },
+        )
+      ]),
+    );
+  }
+}
