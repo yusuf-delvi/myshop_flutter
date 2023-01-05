a285d1fd04762a5d0a7a888423ddccd9353e6ef1
Yusuf Mohammed Delvi <yusufmddelvi8@gmail.com>
2023-01-05 13:00:58 +0530

    confirm alert on removing item from cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -27,6 +27,29 @@ class CartItem extends StatelessWidget {
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('Are you sure?'),
+            content: const Text('Do you want to remove the item from cart?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: const Text('No'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: const Text('Yes'),
+              ),
+            ],
+          ),
+        );
+      },
       background: Container(
         color: Colors.red,
         alignment: Alignment.centerRight,
